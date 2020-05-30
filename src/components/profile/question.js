9b9c902177c3b5fc00728a2e37dc99dedd906a4c
vard0ni <45847565+vard0ni@users.noreply.github.com>
2020-05-30 22:36:13 +0700

    up: update questions.js (#5)

--- src/components/profile/question.js
+++ src/components/profile/question.js
@@ -1,6 +1,6 @@
 export const QUESTIONS = [
   {
-    title: 'Какие жанры в предпочитаете?',
+    title: 'Какие жанры вы предпочитаете?',
     inputs: [
       {
         id: 1,
@@ -11,6 +11,25 @@ export const QUESTIONS = [
         id: 2,
         label: 'Шутеры(Shooter)',
         value: 'Shooter'
+      },
+      {
+        id: 3,
+        label: 'Аркады(Arcade)',
+        value: 'Arcade'
+      },
+      {
+        id: 4,
+        label: 'Cимуляторы(Simulator)',
+        value: 'Simulator'
+      },
+      {
+        id: 5,
+        label: 'Стратегии(Strategy)',
+        value: 'Strategy'
+      },{
+        id: 6,
+        label: 'Гонки(Race)',
+        value: 'Race'
       }
     ]
   },
@@ -18,15 +37,137 @@ export const QUESTIONS = [
     title: 'Какие разработчики вам нравятся?',
     inputs: [
       {
-        id: 3,
-        label: 'Epic',
-        value: 'Epic'
-      },
-      {
-        id: 4,
+        id: 7,
         label: 'Rockstar',
         value: 'Rockstar'
       },
+      {
+        id: 8,
+        label: 'Epic Games',
+        value: 'Epic Games'
+      },
+      {
+        id: 9,
+        label: 'Valve',
+        value: 'Valve'
+      },
+      {
+        id: 10,
+        label: 'Blizzard',
+        value: 'Blizzard'
+      },
+      {
+        id: 11,
+        label: 'Ubisoft',
+        value: 'Ubisoft'
+      },
+      {
+        id: 12,
+        label: ' Electronic Arts',
+        value: ' Electronic Arts'
+      },
     ]
-  }
+  },
+  {
+    title: 'Любите ли вы играть в компаниях?',
+    inputs: [
+      {
+        id: 13,
+        label: 'Да',
+        value: 'multiplayer'
+      },
+      {
+        id: 14,
+        label: 'Нет',
+        value: 'single-player'
+      }
+    ]
+  },
+  {
+    title: 'На каких устройствах вы играете?',
+    inputs: [
+      {
+        id: 15,
+        label: 'ПК',
+        value: 'PC'
+      },
+      {
+        id: 16,
+        label: 'Консоль',
+        value: 'console'
+      },
+      {
+        id: 17,
+        label: 'Мобильный телефон',
+        value: 'mobile phone'
+      }
+    ]
+  },
+  {
+    title: 'Какие игры предпочитаете по дате релиза?',
+    inputs: [
+      {
+        id: 18,
+        label: 'Свежые(cs go, dota 2, valorant)',
+        value: 'New'
+      },
+      {
+        id: 19,
+        label: 'Проверенные(diablo, fallout, stalker, warcraft)',
+        value: 'Proven'
+      }
+    ]
+  },
+  {
+    title: 'Какой у вас приблизительный игровой стаж?',
+    inputs: [
+      {
+        id: 20,
+        label: '< 1 года',
+        value: 'intern'
+      },
+      {
+        id: 21,
+        label: '1-5 лет',
+        value: 'junior'
+      },
+      {
+        id: 22,
+        label: '5-10 лет',
+        value: 'middle'
+      },
+      {
+        id: 23,
+        label: '10-15 лет',
+        value: 'senior'
+      },
+      {
+        id: 24,
+        label: '> 15 лет',
+        value: 'tech-lead'
+      }
+    ]
+  },
+  {
+    title: 'Как часто вы играете?',
+    inputs: [
+      {
+        id: 25,
+        label: 'Каждый день',
+        value: 'every day'
+      },
+      {
+        id: 26,
+        label: '2-3 раза в неделю',
+        value: 'often'
+      },
+      {
+        id: 27,
+        label: 'Раз в неделю',
+        value: 'rarely'
+      },
+
+    ]
+  },
+
 ];
