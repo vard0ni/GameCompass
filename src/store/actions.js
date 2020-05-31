62ec09f071597ec91568b1ff477e7cf2cdea33f4
Ilia Tretiak <ilia.tretiak.2001@mail.ru>
2020-05-31 16:06:35 +0300

    Update actions.js

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,4 @@
 import {API_PARAMS, API_URL} from '../environment/enviroments';
-import {API_URL} from '../environment/enviroments';
 
 export const FETCH = {
   type: 'FETCH'
@@ -17,15 +16,6 @@ export function fetching(params) {
     fetch(url.toString())
       .then(response => response.json())
         .then(data => dispatch({...FETCH, data, params}))
-
-export function fetching(object) {
-  return (dispatch) => {
-    let url = new URL(API_URL);
-    Object.keys(object)
-      .forEach(key => url.searchParams.append(key, object[key]));
-    fetch(url.toString())
-      .then(response => response.json())
-        .then(data => dispatch({...FETCH, data}))
         .catch(er => console.log('Fetching error: ', er));
   }
 }
